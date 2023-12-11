ab2e6f04e167fd08bcfef20315d2598890fbdaa7
muhammadtalhajtech01 <muhammad.talha@jintech.com>
2023-12-11 18:02:24 +0500

    Created directory structure with routes ,controller and models.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import  express  from 'express';
-  
+import connectDb from './utilities/databaseConnecter.js';
+import 'dotenv/config'  
 const app = express(); 
-const PORT = 3000; 
-  
-app.listen(PORT, (error) =>{ 
+
+const port = process.env.PORT || 5000
+connectDb()
+app.listen(port, (error) =>{ 
     if(!error) 
-        console.log("Server is Successfully Running,  and App is listening on port "+ PORT) 
+        console.log("Server is Successfully Running,  and App is listening on port "+ port) 
     else 
         console.log("Error occurred, server can't start", error); 
     } 
---
+++ backend/utilities/databaseConnecter.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+export default function connectDb() {
+  mongoose.set('strictQuery', true)
+  mongoose
+    .connect(process.env.DB_URL, {
+      auth: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+      },
+    
+    })
+    .then(() => {
+      console.debug('Connected to DB')
+    })
+    .catch((err) => {
+      throw `There is an error in connecting DB ${err.message}`
+    })
+}
